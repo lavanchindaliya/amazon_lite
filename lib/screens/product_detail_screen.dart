67c3b01f714d61ac4fdca7d9acb3f096d9566a11
Lavan chindaliya <chindaliyalavan@gmail.com>
2022-07-29 19:14:38 +0530

    added product detail data

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:amazon_lite/provider/products.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -8,11 +10,39 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)!.settings.arguments as String;
-    final title =
-        Provider.of<Products>(context, listen: false).findById(productId).title;
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(loadedProduct.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+                height: 300,
+                width: double.infinity,
+                child:
+                    Image.network(loadedProduct.imageUrl, fit: BoxFit.cover)),
+            SizedBox(
+              height: 10,
+            ),
+            Text('\$${loadedProduct.price}',
+                style: TextStyle(color: Colors.grey, fontSize: 20)),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                loadedProduct.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
