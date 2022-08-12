ef6d5a1622fe417954f17a5077b5f7094cae02e3
Lavan chindaliya <chindaliyalavan@gmail.com>
2022-08-12 10:49:47 +0530

    reduce the code redundancy in auth prvider screen

--- lib/provider/auth.dart
+++ lib/provider/auth.dart
@@ -8,20 +8,20 @@ class Auth with ChangeNotifier {
   late DateTime _expiartyDate;
   late String _userId;
 
-  Future<void> signUp(String? email, String? password) async {
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD3dcMagsyrvw9wwLA0G5pnFeQcEw4jRH0';
+  Future<void> authenticate(
+      String? email, String? password, String urlSegment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyD3dcMagsyrvw9wwLA0G5pnFeQcEw4jRH0';
     final response = await https.post(Uri.parse(url),
         body: json.encode(
             {'email': email, 'password': password, 'returnSecureToken': true}));
   }
 
+  Future<void> signUp(String? email, String? password) async {
+    return await authenticate(email, password, 'signUp');
+  }
+
   Future<void> signIn(String? email, String? password) async {
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD3dcMagsyrvw9wwLA0G5pnFeQcEw4jRH0';
-    final response = await https.post(Uri.parse(url),
-        body: json.encode(
-            {'email': email, 'password': password, 'returnSecureToken': true}));
-    print(json.decode(response.body));
+    return await authenticate(email, password, 'signInWithPassword');
   }
 }
