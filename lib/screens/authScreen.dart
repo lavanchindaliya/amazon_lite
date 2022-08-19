c1916d2e9b0731b7103b53fbf602ba762350cacf
Lavan chindaliya <chindaliyalavan@gmail.com>
2022-08-19 12:56:54 +0530

    added the fadeTransistion and slide transition to signup button

--- lib/screens/authScreen.dart
+++ lib/screens/authScreen.dart
@@ -122,20 +122,25 @@ class _AuthCardState extends State<AuthCard>
 
   var _isLoading = false;
   final _passwordController = TextEditingController();
-  // AnimationController? _controller;
-  // Animation<Size>? _heightAnimation;
+  late AnimationController _controller;
+  late Animation<double> _opacityAnimation;
+  late Animation<Offset> _slideAnimation;
 
   @override
   void initState() {
     super.initState();
-    // _controller =
-    //     AnimationController(vsync: this, duration: Duration(milliseconds: 400));
+    _controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 400));
     // _heightAnimation = Tween<Size>(
     //         begin: Size(double.infinity, 260), end: Size(double.infinity, 320))
     //     .animate(CurvedAnimation(parent: _controller!, curve: Curves.linear));
     // _heightAnimation!.addListener(() {
     //   setState(() {});
     // });
+    _slideAnimation = Tween<Offset>(begin: Offset(0, -1.5), end: Offset(0, 0))
+        .animate(_controller);
+    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0)
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
   }
 
   // @override
@@ -190,12 +195,12 @@ class _AuthCardState extends State<AuthCard>
       setState(() {
         _authMode = AuthMode.Signup;
       });
-      // _controller!.forward();
+      _controller.forward();
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
-      // _controller!.reverse();
+      _controller.reverse();
     }
   }
 
@@ -208,7 +213,7 @@ class _AuthCardState extends State<AuthCard>
         ),
         elevation: 8.0,
         child: AnimatedContainer(
-          duration: Duration(milliseconds: 400),
+          duration: Duration(milliseconds: 300),
           curve: Curves.easeIn,
           height: _authMode == AuthMode.Signup ? 320 : 260,
 
@@ -247,20 +252,33 @@ class _AuthCardState extends State<AuthCard>
                       _authData['password'] = value!;
                     },
                   ),
-                  if (_authMode == AuthMode.Signup)
-                    TextFormField(
-                      enabled: _authMode == AuthMode.Signup,
-                      decoration:
-                          InputDecoration(labelText: 'Confirm Password'),
-                      obscureText: true,
-                      validator: _authMode == AuthMode.Signup
-                          ? (value) {
-                              if (value != _passwordController.text) {
-                                return 'Passwords do not match!';
-                              }
-                            }
-                          : null,
+                  AnimatedContainer(
+                    curve: Curves.easeIn,
+                    constraints: BoxConstraints(
+                      minHeight: _authMode == AuthMode.Signup ? 60 : 0,
+                      maxHeight: _authMode == AuthMode.Signup ? 120 : 0,
                     ),
+                    duration: Duration(milliseconds: 300),
+                    child: FadeTransition(
+                      opacity: _opacityAnimation,
+                      child: SlideTransition(
+                        position: _slideAnimation,
+                        child: TextFormField(
+                          enabled: _authMode == AuthMode.Signup,
+                          decoration:
+                              InputDecoration(labelText: 'Confirm Password'),
+                          obscureText: true,
+                          validator: _authMode == AuthMode.Signup
+                              ? (value) {
+                                  if (value != _passwordController.text) {
+                                    return 'Passwords do not match!';
+                                  }
+                                }
+                              : null,
+                        ),
+                      ),
+                    ),
+                  ),
                   SizedBox(
                     height: 20,
                   ),
